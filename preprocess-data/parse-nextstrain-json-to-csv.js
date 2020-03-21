67517739a18ec55916d68a9f895a07c7e63cf63c
e-ru <erik.rudisch@posteo.de>
2020-03-21 23:21:16 +0100

    fix parsing for top level nodes in parse-nextstrain-json-to-csv.js

--- preprocess-data/parse-nextstrain-json-to-csv.js
+++ preprocess-data/parse-nextstrain-json-to-csv.js
@@ -26,7 +26,7 @@ const header = [
 
 let rows = [];
 
-json.tree.children.forEach(processTreeNode);
+processTreeNode(json.tree, rows);
 
 rows = rows.filter(row => !!row).map(row => {
   return [
@@ -53,71 +53,68 @@ rows = rows.filter(row => !!row).map(row => {
   .join(', ');
 
 }).join('\n');
-fs.writeFileSync(outputFile, header.join(', ')+'\n'+rows)
+fs.writeFileSync(outputFile, header.join(', ')+'\n'+rows+'\n');
 
-function processTreeNode(node, parent){
-  let entry = {};
-  // mutation name
-  entry['name'] = node.name;
-  entry['parent'] = parent;
+function processTreeNode(tree, rows){
+  if (tree.children && tree.children.length > 0) {    
+    tree.children.forEach((node) => { 
+      let entry = {};
+      // mutation name
+      entry['name'] = node.name;
+      entry['parent'] = tree.name;
 
-  // get division
-  entry['division'] = node.node_attrs.division.value;
+      // get division
+      entry['division'] = node.node_attrs.division.value;
 
-  // get locations
-  if (divisions[entry.division]) {
-    entry['long'] = divisions[entry.division].longitude;
-    entry['lat'] = divisions[entry.division].latitude;
-  }
-  
-  // country if exists
-  if (node.node_attrs && node.node_attrs.country) {
-    entry['country'] = node.node_attrs.country.value;
-  }
+      // get locations
+      if (divisions[entry.division]) {
+        entry['long'] = divisions[entry.division].longitude;
+        entry['lat'] = divisions[entry.division].latitude;
+      }
+      
+      // country if exists
+      if (node.node_attrs && node.node_attrs.country) {
+        entry['country'] = node.node_attrs.country.value;
+      }
 
-  // get clade if exists
-  if (node.node_attrs.clade_membership) {
-    entry['clade'] = node.node_attrs.clade_membership.value;
-  } else if (node.branch_attrs && node.branch_attrs.labels && node.branch_attrs.labels.clade) {
-    entry['clade'] = node.branch_attrs.labels.clade;
-  }
-  // get labs if exists
-  if (node.node_attrs.originating_lab) {
-    entry['originating_lab'] = node.node_attrs.originating_lab.value;
-  }
-  if (node.node_attrs.submitting_lab) {
-    entry['submitting_lab'] = node.node_attrs.submitting_lab.value;
-  }
+      // get clade if exists
+      if (node.node_attrs.clade_membership) {
+        entry['clade'] = node.node_attrs.clade_membership.value;
+      } else if (node.branch_attrs && node.branch_attrs.labels && node.branch_attrs.labels.clade) {
+        entry['clade'] = node.branch_attrs.labels.clade;
+      }
+      // get labs if exists
+      if (node.node_attrs.originating_lab) {
+        entry['originating_lab'] = node.node_attrs.originating_lab.value;
+      }
+      if (node.node_attrs.submitting_lab) {
+        entry['submitting_lab'] = node.node_attrs.submitting_lab.value;
+      }
 
-  // sampling_date if exists
-  if (node.node_attrs.num_date) {
-    entry['sampling_date'] = node.node_attrs.num_date.value;
-  }
-  
-  // get mutations if exists
-  if (node.branch_attrs && node.branch_attrs.labels) {
-    entry['mutations'] = node.branch_attrs.labels.aa;
-  }
-  // get nuc as string if exists
-  if (node.branch_attrs && node.branch_attrs.mutations && node.branch_attrs.mutations.nuc) {
-    entry['nuc'] = node.branch_attrs.mutations.nuc.join('-');
-  }
+      // sampling_date if exists
+      if (node.node_attrs.num_date) {
+        entry['sampling_date'] = node.node_attrs.num_date.value;
+      }
+      
+      // get mutations if exists
+      if (node.branch_attrs && node.branch_attrs.labels) {
+        entry['mutations'] = node.branch_attrs.labels.aa;
+      }
+      // get nuc as string if exists
+      if (node.branch_attrs && node.branch_attrs.mutations && node.branch_attrs.mutations.nuc) {
+        entry['nuc'] = node.branch_attrs.mutations.nuc.join('-');
+      }
 
-  // get genbank and gisaid if exists
-  if (node.node_attrs.genbank_accession) {
-    entry['genbank'] = node.node_attrs.genbank_accession.value;
-  }
-  if (node.node_attrs.gisaid_epi_isl) {
-    entry['gisaid'] = node.node_attrs.gisaid_epi_isl.value;
-  }
+      // get genbank and gisaid if exists
+      if (node.node_attrs.genbank_accession) {
+        entry['genbank'] = node.node_attrs.genbank_accession.value;
+      }
+      if (node.node_attrs.gisaid_epi_isl) {
+        entry['gisaid'] = node.node_attrs.gisaid_epi_isl.value;
+      }
 
-  rows.push(entry);
-
-  // process children
-  if (node.children && node.children.length > 0) {
-    node.children.map(child => processTreeNode(child, node.name))
-      .forEach(arr => {
-        rows = rows.concat(arr);
-      });
+      rows.push(entry);
+      processTreeNode(node, rows);
+    });
   }
 }
