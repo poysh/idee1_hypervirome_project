2fae520cf107902cd621648446514a9643900335
Chris Ulpinnis <culpinnis@gmail.com>
2020-03-21 19:25:11 +0100

    Update blast-to-snp.sh

--- preprocess-data/blast-to-snp/blast-to-snp.sh
+++ preprocess-data/blast-to-snp/blast-to-snp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  blastn -db genomic/Viruses/Betacoronavirus -query reference.gb -remote -out blast.xml -outfmt 5 -ungapped
- java -jar blastn2snp.jar blast.xml > test.out
+ java -jar blastn2snp.jar blast.xml > snp.txt
  
